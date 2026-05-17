32a35159278bc8b11cec5111f725c010d353a385
simonys32 <91671425+simonys32@users.noreply.github.com>
2026-05-17 08:43:52 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello FS2026 DevOps Course!!! :-) Azure Test with Docker and GH action, FIRST CHANGE 2 SECOND CHANGE 3!');
+  res.send('Hello FS2026 DevOps Course!!! :-) Azure Test with Docker and GH action, FIRST CHANGE 2 SECOND CHANGE 3 CHANGE 4 TEST AZURE!');
 });
 
 app.listen(PORT, HOST);
